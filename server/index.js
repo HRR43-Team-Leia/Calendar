3e50cff7e77556f1dad99098ecd6fed603c2b08f
ga-possum <aleks.khristof@gmail.com>
2020-02-07 00:51:49 -0500

    ready to deploy

--- server/index.js
+++ server/index.js
@@ -7,15 +7,25 @@ const db = require('../database');
 const port = process.env.PORT || 3005;
 
 const app = express();
-app.use(cors({
-  origin: 'http://18.223.132.12:4000',
 
-}));
+const whitelist = ['http://18.223.132.12:4000', 'http://18.222.165.232:4000']
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+
 app.use(express.static('public'));
 app.use(bp.json());
 // app.engine('html', ejs.renderFile);
 
-app.get('/:id', (req, res) => {
+app.get('/:id', cors(corsOptions), (req, res) => {
   res.render('../public/index.html');
 });
 
