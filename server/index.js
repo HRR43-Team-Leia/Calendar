7151c474790b748269f0f113ae90137a52c9e454
ga-possum <aleks.khristof@gmail.com>
2020-02-08 19:51:25 -0500

    00

--- server/index.js
+++ server/index.js
@@ -8,10 +8,25 @@ const port = process.env.PORT || 3005;
 
 const app = express();
 
+//add same address:3005
+//add Dan:4000
 
-app.use(cors({
-  origin: 'http://18.223.132.12:4000',
-}));
+const whitelist = [
+  'http://18.222.165.232:4000', //Dan proxy
+  'http://18.223.132.12:4000', //my proxy
+  'http://18.217.113.225:4000', //Yer proxy
+  'http://18.223.132.12:3005'
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 app.use(express.static('public'));
 app.use(bp.json());
 // app.engine('html', ejs.renderFile);
@@ -20,9 +35,9 @@ app.get('/:id', (req, res) => {
   res.render('../public/index.html');
 });
 
-app.get('/calendar/:id/', (req, res) => {
+app.get('/calendar/:id/', cors(corsOptions), (req, res) => {
   db.get(req.params.id,
-    () => { res.sendStatus(500); },
+    () => { res.sendStatus(400); },
     (data) => { res.send(data); });
 });
 
